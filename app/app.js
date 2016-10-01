8679937804032430cf8e8b6e4d1222be3da97b80
Exponent500 <reuvenkishon@gmail.com>
2016-09-30 20:01:26 -0700

    cleaned up code

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-//creates an Angular module. First parameter is the name of the app. This should match the ng-app attribute that is included within the html tag. Second parameter is for dependencies. In this case it's a google maps for angular (?)
+//create Angular module with a dependency of angular google maps
 var myApp = angular.module('sanDiegoTopSpots', ['uiGmapgoogle-maps']); 
 
-//creates an Angular controller. It says lets build a controller (MainController) and inject some things into it. One of those things is $scope. $scope allows us to bind data to the view (or HTML). Another is $http, which allows us, among other things, to pull data from a JSON file. Finally there is a handler function, which gets $scope and $html passed into it so it can use it within the function.
+//create Angular controller and inject scope/http into it
 myApp.controller('MainController', ['$scope', '$http', function($scope, $http){ 
 
   //creates the default view of the google map upon initial load
@@ -13,17 +13,16 @@ myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
     zoom: 12
   };
 
-  $scope.markers = [];
+  var markers = [];
 
   //populates our HTML table with data from within topspots.json and adds google maps markers to the google map
   $http.get('topspots.json') //http.get is a method to grab data from an external local json file (amongst other things)
     .then(function(result) { //stores data that was grabbed from topspots.json and places it into result
-      $scope.topspots = result.data.topspots; //creates a new $scope variable that contains all the data within the local json file
-      $scope.lat = $scope.topspots[0];
-      $scope.long = result.data.topspots;
-      var markers = result.data.topspots;
-      _.forEach(markers, function(marker){ //grabs lat/lon data from json file and uses it to create the google maps markers
-        marker.coords= {
+      markers = result.data.topspots;
+      $scope.topspots = markers; //creates a new $scope variable that contains all the data within the local json file
+      
+      _.forEach(markers, function(marker){ //grabs lat/lon data from json file and uses it to create the google maps markers.
+        marker.coords= { 
           latitude: marker.location[0],
           longitude: marker.location[1]
         };
@@ -31,25 +30,18 @@ myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
     $scope.markers = markers;
     });
 
-
-    // $scope.location = [];
-    var arr=[];
+    var location=[];
 
     //adds a row to the table after submitting a form
     $scope.addRow = function(){
       
-      arr[0] = $scope.latitude;
-      arr[1] = $scope.longitude;
-      // $scope.location.push($scope.latitude); //pushes the latitude value entered by user, into the location array
-      // $scope.location.push($scope.longitude);
+      location[0] = $scope.latitude;
+      location[1] = $scope.longitude;
       $scope.topspots.push(
         { 
         'name':$scope.name,
         'description': $scope.description,
-        'location': arr
-
-        // 'latitude': $scope.latitude,
-        // 'longitude': $scope.longitude
+        'location': location
       });
 
       //clears these scope variables after adding a row
@@ -57,12 +49,6 @@ myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
       $scope.description='';
       $scope.location= '';
     };
-    // //process form data and tries to add it to the local topspots.json
-    // $scope.processForm = function() {
-    //   $http.post('topspots.json', $scope.formData).success(function(data) {
-    //     console.log(data);
-    //   });
-    // };
 }]);
 
 
